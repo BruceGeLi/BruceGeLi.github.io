8e012d973cca294e9ec6f645380fdc78954f565b
ge.li <ge.li@kit.edu>
2024-02-08 00:06:18 +0100

    redo the emoji

--- assets/theme/theme-switcher.js
+++ assets/theme/theme-switcher.js
@@ -1,32 +1,22 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
     document.body.classList.add(`${savedTheme}-theme`);
-
-    // Update icon visibility based on the saved theme or system preference
-    updateIconVisibility(savedTheme);
+    updateLabelVisibility(savedTheme);
 });
 
-function updateIconVisibility(theme) {
-    document.getElementById('icon-light-theme').style.display = theme === 'dark' ? 'block' : 'none';
-    document.getElementById('icon-dark-theme').style.display = theme === 'light' ? 'block' : 'none';
-}
-
-// Function to switch theme and save preference
 function switchTheme() {
     const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
 
-    // Toggle theme class on body
-    document.body.classList.remove(`${currentTheme}-theme`);
-    document.body.classList.add(`${newTheme}-theme`);
-
-    // Save the user's theme preference
+    document.body.classList.replace(`${currentTheme}-theme`, `${newTheme}-theme`);
     localStorage.setItem('theme', newTheme);
-
-    // Update icon visibility
-    updateIconVisibility(newTheme);
+    updateLabelVisibility(newTheme);
 }
 
-// Event listener for the button
+function updateLabelVisibility(theme) {
+    document.getElementById('light-label').style.display = theme === 'dark' ? 'block' : 'none';
+    document.getElementById('dark-label').style.display = theme === 'light' ? 'block' : 'none';
+}
+
 const toggleButton = document.getElementById('theme-toggle');
 toggleButton.addEventListener('click', switchTheme);
